b0754f87b18037edc5ee86e0515944a96ae31acc
franco garcia <francoagarcia@gmail.com>
2014-05-10 20:45:39 -0300

    Script de creacion inicial completo. Crea el esquema y todas las tablas del DER. Y toque una cosita en el gitignore

--- FrbaCommerce/SQL/script_creacion_inicial.sql
+++ FrbaCommerce/SQL/script_creacion_inicial.sql
@@ -1,14 +1,3 @@
-/* 
-****************************************************************************************************
-**********************************************NOTA**************************************************
-****************************************************************************************************
-
-	1) Me falta crear las tablas Factura e Item_factura pero estoy averiguando bien como hacerlo porque me queda dudas todavia
-	  con respecto a la forma de pago y no quiero crear las tablas hasta que esten bien
-	2) Le puse el nombre PEPITO al esquema por ahora, porque no sabia que ponerle, creo que se le ponia el nombre del grupo o algo asi
-	3) En el DER que genera el SQL MANAGEMENT las "llaves" son relacion de a 1, y los "rombos" son de muchos
-
-*/
 
 -------------------------------------------------------------------------------------
 ---------------------------------CREANDO EL ESQUEMA----------------------------------
@@ -17,75 +6,121 @@ BEGIN TRANSACTION --Comienza transaccion
 
 USE [GD1C2014]
 GO
-CREATE SCHEMA [PEPITO] AUTHORIZATION [gd]
-GO
 
+DECLARE @SchemaName AS VARCHAR(100) = 'PEPITO'
+DECLARE @sql VARCHAR(100) = 'CREATE SCHEMA '+ @SchemaName +' AUTHORIZATION [gd]'
+
+IF NOT EXISTS(SELECT name FROM sys.schemas WHERE name = @SchemaName)
+	BEGIN
+		PRINT 'Creating ' + @SchemaName + ' schema'
+		EXEC(@sql)
+		PRINT @SchemaName + ' schema created'
+	END
+ELSE
+  PRINT @SchemaName + ' schema already exists.'
 -------------------------------------------------------------------------------------
 ---------------------------------CREANDO LAS TABLAS----------------------------------
 -------------------------------------------------------------------------------------
 
+IF OBJECT_ID('PEPITO.Rol', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Rol;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Rol(
-	id_rol NUMERIC(18,0) IDENTITY(1,1) NOT NULL, --IDENTITY(valor 1° fila, increment) para hacer que sea auto-increment
+	id_rol NUMERIC(18,0) IDENTITY(1,1) NOT NULL, 
 	nombre varchar(255),
-	habilitada bit DEFAULT 1, --Por default esta habilitada
+	habilitada bit DEFAULT 1, 
 );
 
+
+
+IF OBJECT_ID('PEPITO.Funcionalidad', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Funcionalidad;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Funcionalidad (
 	id_funcionalidad NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
 	nombre varchar(255),
 	habilitada bit DEFAULT 1,
 );
 
+IF OBJECT_ID('PEPITO.FuncionalidadXRol', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.FuncionalidadXRol;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.FuncionalidadXRol (
 	id_rol NUMERIC(18,0) NOT NULL,
 	id_funcionalidad NUMERIC(18,0) NOT NULL,
 	habilitada bit DEFAULT 1,
 );
 
+IF OBJECT_ID('PEPITO.Usuario', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Usuario;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Usuario (
 	id_usuario NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
 	username varchar(255),
 	contrasenia varchar(255),
 	telefono NUMERIC(18,0),
-	intentos_login NUMERIC(1, 0) DEFAULT 0, --Lo pongo aqui nomas al contador de intentos
-	tipo_usario varchar(3), --Cli, adm, o emp, 
+	intentos_login NUMERIC(1, 0) DEFAULT 0, 
+	tipo_usario varchar(3), 
 	habilitada bit DEFAULT 1,
 );
 
-CREATE TABLE PEPITO.Administrador ( --TODO ver que mas poner aqui
-	id_usuario NUMERIC(18,0) NOT NULL, --FK de usuario
+IF OBJECT_ID('PEPITO.Administrador', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Administrador;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
+CREATE TABLE PEPITO.Administrador ( 
+	id_usuario NUMERIC(18,0) NOT NULL, 
 	nombre nvarchar(255),
 	apellido nvarchar(255),
 	habilitada bit DEFAULT 1,
 );
 
+IF OBJECT_ID('PEPITO.Cliente', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Cliente;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Cliente (
-	id_usuario NUMERIC(18,0) NOT NULL, --FK usuario && PK cliente
-	nombre nvarchar(255), --En la tabla MAESTRA dice tipo de dato nvarchar, no varchar
+	id_usuario NUMERIC(18,0) NOT NULL,
+	nombre nvarchar(255), 
 	apellido nvarchar (255),
-	tipo_documento varchar(255), --Tal vez pueda crear una tabla "Tipo documento" y aqui poner la clave
+	tipo_documento varchar(255), 
 	numero_documento NUMERIC(18,0),
-	dom_calle nvarchar(255), --Nombre de calle ---> NORMALIZAR LAS DIRECCIONES ?
-	piso NUMERIC(18,0),      --Piso del dpto
-	depto nvarchar(50),      --Numeracion del Depto
+	dom_calle nvarchar(255),
+	nro_calle NUMERIC(18, 0),
+	piso NUMERIC(18,0),      
+	depto nvarchar(50),      
 	localidad varchar(255),
-	nro_calle NUMERIC(18,0), --Este campo esta en la tabla MAESTRA pero no veo para que dejarlo, es al dope me parece
 	cod_postal nvarchar(50),
-	mail nvarchar(255), --Tiene distinto tipo de datos que el usuario EMPRESA
+	mail nvarchar(255),
 	fecha_nacimiento datetime,
 	sexo bit DEFAULT NULL,
 	habilitada bit DEFAULT 1,
 );
 
+IF OBJECT_ID('PEPITO.Empresa', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Empresa;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Empresa (
-	id_usuario NUMERIC(18,0) NOT NULL, --FK usuario && PK cliente
-	razon_social nvarchar(255), --En la tabla MAESTRA dice tipo de dato nvarchar, no varchar
-	mail nvarchar(50), --Tiene distinto tipo de datos que el usuario EMPRESA
+	id_usuario NUMERIC(18,0) NOT NULL, 
+	razon_social nvarchar(255), 
+	mail nvarchar(50),
 	dom_calle nvarchar(255),
 	piso NUMERIC(18,0) , 
 	depto nvarchar(50),
 	localidad nvarchar(255),
-	nro_calle NUMERIC(18,0), --Este campo esta en la tabla MAESTRA pero no veo para que dejarlo, es al dope me parece
+	nro_calle NUMERIC(18,0),
 	cod_postal nvarchar(50),
 	ciudad nvarchar(255),
 	CUIT nvarchar(50),
@@ -93,40 +128,75 @@ CREATE TABLE PEPITO.Empresa (
 	fecha_creacion datetime,
 );
 
+IF OBJECT_ID('PEPITO.UsuarioXRol', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.UsuarioXRol;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.UsuarioXRol (
-	id_usuario NUMERIC(18,0) NOT NULL, --PK compuesta: id_usuario&&id_rol --Ademas estas dos son FK de su tabla correspondiente
-	id_rol NUMERIC(18,0) NOT NULL, --PK
+	id_usuario NUMERIC(18,0) NOT NULL, 
+	id_rol NUMERIC(18,0) NOT NULL,
 	habilitada bit DEFAULT 1,
 );
 
-CREATE TABLE PEPITO.Rubro( --TODO agregar esto a publicacion, relaciond e muchos a muchos
-	id_rubro NUMERIC(18,0) IDENTITY(1,1) NOT NULL, --PK
+IF OBJECT_ID('PEPITO.Rubro', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Rubro;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
+CREATE TABLE PEPITO.Rubro(
+	id_rubro NUMERIC(18,0) IDENTITY(1,1) NOT NULL, 
 	descripcion nvarchar(255),
 );
 
+IF OBJECT_ID('PEPITO.TipoPublicacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.TipoPublicacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.TipoPublicacion(
-	id_tipo_publicacion NUMERIC(18,0) IDENTITY(1,1) NOT NULL, --PK
+	id_tipo_publicacion NUMERIC(18,0) IDENTITY(1,1) NOT NULL, 
 	descripcion nvarchar(255),
 );
 
+IF OBJECT_ID('PEPITO.VisibilidadPublicacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.VisibilidadPublicacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.VisibilidadPublicacion(
-	id_visibilidad NUMERIC(18,0) IDENTITY(1,1) NOT NULL, --PK
+	id_visibilidad NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
 	descripcion nvarchar(255),
 	precio NUMERIC(18, 2),
 	porcentaje NUMERIC(18, 2),
 );
 
+IF OBJECT_ID('PEPITO.EstadoPublicacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.EstadoPublicacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.EstadoPublicacion(
-	id_estado NUMERIC(18,0) IDENTITY(1,1) NOT NULL, --PK
+	id_estado NUMERIC(18,0) IDENTITY(1,1) NOT NULL, 
 	descripcion nvarchar(255),
 );
 
+IF OBJECT_ID('PEPITO.Calificacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Calificacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Calificacion(
-	id_calificacion NUMERIC(18,0) IDENTITY(1,1) NOT NULL, --PK
+	id_calificacion NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
 	cantidad_estrellas NUMERIC(18, 0),
 	descripcion nvarchar(255),
 );
 
+IF OBJECT_ID('PEPITO.Publicacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Publicacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Publicacion(
 	id_publicacion NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
 	descripcion nvarchar(255),
@@ -134,49 +204,103 @@ CREATE TABLE PEPITO.Publicacion(
 	fecha_inicio datetime,
 	fecha_vencimiento datetime,
 	precio NUMERIC(18,2),
-	id_tipo_publicacion NUMERIC(18,0), --FK
-	id_visibilidad NUMERIC(18,0), --FK
-	id_estado NUMERIC(18,0), --FK
+	id_tipo_publicacion NUMERIC(18,0),
+	id_visibilidad NUMERIC(18,0), 
+	id_estado NUMERIC(18,0), 
 	id_usuario_publicador NUMERIC(18,0),
 	habilitada bit DEFAULT 1,
 );
 
+IF OBJECT_ID('PEPITO.Pregunta', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Pregunta;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Pregunta(
 	id_pregunta NUMERIC(18,0) IDENTITY(1,1) NOT NULL,
-	id_publicacion NUMERIC(18,0), --FK
-	id_usuario NUMERIC(18, 0), --FK: Usuario que pregunta
+	id_publicacion NUMERIC(18,0),
+	id_usuario NUMERIC(18, 0),
 	pregunta nvarchar(255),
+	fecha_pregunta datetime,
 	respuesta nvarchar(400),
+	fecha_respuesta datetime,
 );
 
---TODO guardarme tambien las OFERTAS
-CREATE TABLE PEPITO.Compra( --PK compuesta por:	id_publicacion&&id_usuario_comprador => ademas son FK de sus tablas
-	id_compra NUMERIC(18,0) NOT NULL, --PK
-	id_publicacion NUMERIC(18,0), --FK
-	id_usuario_comprador NUMERIC(18,0), --FK
-	id_calificacion NUMERIC(18,0), --FK
+IF OBJECT_ID('PEPITO.CalificacionPublicacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.CalificacionPublicacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
+CREATE TABLE PEPITO.CalificacionPublicacion(
+	id_publicacion NUMERIC(18, 0) NOT NULL,
+	id_usuario NUMERIC(18, 0) NOT NULL,
+	id_calificacion NUMERIC(18, 0),
 	detalle_calificacion nvarchar(255),
-	fecha datetime,
-	cantidad numeric(18, 0),
 );
 
+IF OBJECT_ID('PEPITO.Compra', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Compra;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
+CREATE TABLE PEPITO.Compra (
+	id_compra NUMERIC(18, 0) IDENTITY(1,1) NOT NULL,
+	id_publicacion NUMERIC(18, 0),
+	id_usuario_comprador NUMERIC(18, 0),
+	fecha datetime,
+	cantidad NUMERIC(18, 0),
+);
+
+IF OBJECT_ID('PEPITO.Oferta', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Oferta;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
+CREATE TABLE PEPITO.Oferta(
+	id_oferta NUMERIC(18, 0) IDENTITY(1,1) NOT NULL,
+	id_publicacion NUMERIC(18, 0),
+	id_usuario_ofertador NUMERIC(18, 0),
+	fecha datetime,
+	monto NUMERIC(18, 2),
+);
+
+IF OBJECT_ID('PEPITO.RubroXPublicacion', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.RubroXPublicacion;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.RubroXPublicacion(
-	id_rubro NUMERIC(18, 0) NOT NULL, --PK && FK
-	id_publicacion NUMERIC(18, 0) NOT NULL, --PK && FK
+	id_rubro NUMERIC(18, 0) NOT NULL, 
+	id_publicacion NUMERIC(18, 0) NOT NULL, 
 	habilitada bit DEFAULT 1,
 );
-/*
+
+
+IF OBJECT_ID('PEPITO.Factura', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.Factura;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.Factura(
-nro_factura NUMERIC(18,0) NOT NULL,
-fecha,
-total,
-vendedor,
+	nro_factura NUMERIC(18, 0) NOT NULL,
+	id_vendedor NUMERIC(18, 0),
+	fecha datetime,
+	total NUMERIC(18, 2),
+	forma_pago_descripcion nvarchar(255),
 );
 
+
+IF OBJECT_ID('PEPITO.ItemFactura', 'U') IS NOT NULL 
+	BEGIN
+		DROP TABLE PEPITO.ItemFactura;
+		PRINT 'Table already exists: DROP TABLE and CREATE again.'
+	END
 CREATE TABLE PEPITO.ItemFactura(
-
+	nro_factura NUMERIC(18, 0) NOT NULL,
+	id_publicacion NUMERIC(18, 0) NOT NULL,
+	cantidad NUMERIC(18, 0),
+	monto NUMERIC(18, 2),
 );
-*/
 
 -------------------------------------------------------------------------------------
 ----------------------------------CREANDO LAS PKS------------------------------------
@@ -198,7 +322,12 @@ ALTER TABLE PEPITO.EstadoPublicacion ADD CONSTRAINT pk_id_estado PRIMARY KEY ( i
 ALTER TABLE PEPITO.Calificacion ADD CONSTRAINT pk_id_calificacion PRIMARY KEY ( id_calificacion );
 ALTER TABLE PEPITO.Publicacion ADD CONSTRAINT pk_id_publicacion PRIMARY KEY ( id_publicacion );
 ALTER TABLE PEPITO.Pregunta ADD CONSTRAINT pk_id_pregunta PRIMARY KEY ( id_pregunta );
-ALTER TABLE PEPITO.Compra ADD CONSTRAINT pk_publicacion_comprador PRIMARY KEY ( id_compra );
+ALTER TABLE PEPITO.Compra ADD CONSTRAINT pk_id_compra PRIMARY KEY (id_compra);
+ALTER TABLE PEPITO.Oferta ADD CONSTRAINT pk_id_oferta PRIMARY KEY (id_oferta);
+ALTER TABLE PEPITO.CalificacionPublicacion ADD CONSTRAINT pk_id_calificacion_publicacion PRIMARY KEY(id_publicacion);
+ALTER TABLE PEPITO.Factura ADD CONSTRAINT pk_nro_factura PRIMARY KEY (nro_factura);
+ALTER TABLE PEPITO.ItemFactura ADD CONSTRAINT pk_item_factura PRIMARY KEY (nro_factura, id_publicacion);
+
 
 -------------------------------------------------------------------------------------
 ----------------------------------CREANDO LAS FKS------------------------------------
@@ -254,30 +383,39 @@ ALTER TABLE PEPITO.Pregunta ADD CONSTRAINT fk_Pregunta_to_Usuario
 FOREIGN KEY (id_usuario) REFERENCES PEPITO.Usuario (id_usuario);
 
 --Compra
-ALTER TABLE PEPITO.Compra ADD CONSTRAINT fk_Compra_to_Publicacion
+ALTER TABLE PEPITO.Compra ADD CONSTRAINT fk_Compra_to_Publicacion 
 FOREIGN KEY (id_publicacion) REFERENCES PEPITO.Publicacion (id_publicacion);
 
-ALTER TABLE PEPITO.Compra ADD CONSTRAINT fk_Compra_to_Usuario
+ALTER TABLE PEPITO.Compra ADD CONSTRAINT fk_Compra_to_Usuario 
 FOREIGN KEY (id_usuario_comprador) REFERENCES PEPITO.Usuario (id_usuario);
 
-ALTER TABLE PEPITO.Compra ADD CONSTRAINT fk_Compra_to_Calificacion
+--CalificacionPublicacion
+ALTER TABLE PEPITO.CalificacionPublicacion ADD CONSTRAINT fk_CalificacionPublicacion_to_Publicacion 
+FOREIGN KEY (id_publicacion) REFERENCES PEPITO.Publicacion (id_publicacion);
+
+ALTER TABLE PEPITO.CalificacionPublicacion  ADD CONSTRAINT fk_CalificacionPublicacion_to_Usuario 
+FOREIGN KEY (id_usuario) REFERENCES PEPITO.Usuario (id_usuario);
+
+ALTER TABLE PEPITO.CalificacionPublicacion  ADD CONSTRAINT fk_CalificacionPublicacion_to_Calificacion 
 FOREIGN KEY (id_calificacion) REFERENCES PEPITO.Calificacion (id_calificacion);
+
+--Oferta
+ALTER TABLE PEPITO.Oferta ADD CONSTRAINT fk_Oferta_to_Publicacion 
+FOREIGN KEY (id_publicacion) REFERENCES PEPITO.Publicacion (id_publicacion);
+
+ALTER TABLE PEPITO.Oferta ADD CONSTRAINT fk_Oferta_to_Usuario 
+FOREIGN KEY (id_usuario_ofertador) REFERENCES PEPITO.Usuario (id_usuario);
+
+--Facturas
+ALTER TABLE PEPITO.Factura ADD CONSTRAINT fk_Factura_to_Usuario 
+FOREIGN KEY (id_vendedor) REFERENCES PEPITO.Usuario (id_usuario);
+
+ALTER TABLE PEPITO.ItemFactura ADD CONSTRAINT fk_ItemFactura_to_Publicacion 
+FOREIGN KEY (id_publicacion) REFERENCES PEPITO.Publicacion ( id_publicacion );
+
+ALTER TABLE PEPITO.ItemFactura ADD CONSTRAINT fk_ItemFactura_to_Factura 
+FOREIGN KEY (nro_factura) REFERENCES PEPITO.Factura ( nro_factura );
+
 
 --Completo la transaccion
 COMMIT
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
